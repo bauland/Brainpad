a4cf7138436b0029075a378b498fc9247a95e312
Nicolas Bauland <nicolas.bauland@laposte.net>
2017-08-26 11:05:40 +0200

    Initial commit

---
+++ BlinkTest_BP2/BlinkTest_BP2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BlinkTest_BP2
+{
+    class Program
+    {
+        public void BrainPadSetup()
+        {
+            //Put your setup code here. It runs once when the BrainPad starts up.
+
+            BrainPad.Display.DrawTextAndShowOnScreen(0, 0, "Hello!");
+        }
+
+        public void BrainPadLoop()
+        {
+            //Put your program code here. It runs repeatedly after the BrainPad starts up.
+
+            BrainPad.LightBulb.TurnBlue();
+            BrainPad.Wait.Seconds(1);
+            BrainPad.LightBulb.TurnOff();
+            BrainPad.Wait.Seconds(1);
+        }
+    }
+}
